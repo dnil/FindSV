9eaccf1fbfaf58ff69b1fec87b8d119c6b144b1f
J35P312 <jesper.eisfeldt@scilifelab.se>
2015-08-05 21:48:32 +0200

    modified:   FindSV.py 	new file:   annotationconfig.txt 	new file:   filterconfig.txt 	new file:   modules/annotation.py 	new file:   modules/applyFilter.py 	renamed:    modules/scripts.py -> modules/calling/scripts.py 	renamed:    modules/slurm_job.py -> modules/calling/slurm_job.py 	new file:   modules/filter/filterScripts.py 	new file:   modules/findVariant.py 	modified:   modules/readConfigFile.py 	new file:   programFiles/FindTranslocations

---
+++ modules/annotation.py
@@ -0,0 +1,6 @@
+
+#function used to annotate the filtered variants
+def annotation(programDirectory,filtered,processed):
+        print("annotation!");
+        finished=1;
+        return(finished);

655fb40b69ca1588020bd507a5621fd64b3bffb5
J35P312 <jesper.eisfeldt@scilifelab.se>
2015-10-12 22:41:05 +0200

    new file:   modules/combine.py 	new file:   modules/combine/combineScript.py

---
+++ modules/combine.py
@@ -0,0 +1,56 @@
+import sys, os, glob, time
+sys.path.append("modules/combine")
+import common
+import combineScript
+
+
+
+#function used to annotate the filtered variants
+def combine(programDirectory,previousProcessFiles,processed,account):
+       
+        #read the process files
+        processFiles=common.readProcessFiles(["FindSV"],processed,"combine")
+        samplesToMerge={};
+        #combinedProcessFile={}
+        firstTool= True
+        #find all samples that has been analysed by the callers
+        for tools in previousProcessFiles: 
+            for sample in previousProcessFiles[tools]["analysed"]:
+                if firstTool:
+
+                    #combinedProcessFile[sample].update(samplepreviousProcessFiles[tools][sample])
+                    samplesToMerge.update({sample:1})
+                elif( sample in samplesToMerge):
+                    samplesToMerge[sample]+=1
+            firstTool=False
+        
+        #merge the caller results of each sample that has been analysed by all available callers
+        for sample in samplesToMerge:
+            if samplesToMerge[sample] == len(previousProcessFiles):
+                #check the status of each sample
+                if sample in processFiles["FindSV"]["ongoing"]:
+                    done=common.get_slurm_job_status(int(processFiles["FindSV"]["ongoing"][sample]["pid"]))
+                    processFiles=common.get_process_status(done,processFiles,"FindSV",sample)
+                elif sample in processFiles["FindSV"]["failed"].keys():
+                    print "sample {0} FAILED".format(sample)
+                elif sample in processFiles["FindSV"]["cancelled"].keys():
+                    print "sample {0} CANCELLED".format(sample)
+                elif sample in processFiles["FindSV"]["timeout"].keys():
+                    print "sample {0} TIMEOUT".format(sample)
+                elif sample in processFiles["FindSV"]["excluded"].keys():
+                    print "sample {0} EXCLUDED".format(sample)
+                elif sample in processFiles["FindSV"]["analysed"].keys():
+                    print "sample {0} COMPLETE".format(sample)
+                else:
+                    print("submitting: " + sample);
+                    combinedProcessFile={}
+                    tool="";
+                    for tools in previousProcessFiles:
+                        tool = tools
+                        combinedProcessFile.update({tools:previousProcessFiles[tools]["analysed"][sample]})
+                    outgoing=combineScript.submit4combination(tool,sample,combinedProcessFile,programDirectory,account);
+                    processFiles["FindSV"]["ongoing"].update(outgoing)
+       
+        common.UpdateProcessFiles(processFiles,processed,"combine")  
+        finished=1;
+        return(processFiles);
---
+++ modules/combine/combineScript.py
@@ -0,0 +1,56 @@
+import os,subprocess,re,sys
+
+
+def submit4combination(tools,sample,combinedProcessFile,programDirectory,account):
+    sys.path.append(os.path.join(programDirectory,"modules"))
+    import common
+
+    outpath=os.path.join(combinedProcessFile[tools]["outpath"],"FindSV")
+    sbatch_dir,out_dir,err_dir=common.createFolder(outpath);
+    RTGpath=os.path.join(programDirectory,"programFiles","RTG","rtg")
+
+    with open(os.path.join(sbatch_dir, "{}.slurm".format(sample)), 'w') as sbatch:
+        sbatch.write("#! /bin/bash -l\n")
+        sbatch.write("#SBATCH -A {}\n".format(account))
+        sbatch.write("#SBATCH -o {}/combine_{}.out\n".format(out_dir,sample))
+        sbatch.write("#SBATCH -e {}/combine_{}.err\n".format(err_dir,sample))
+        sbatch.write("#SBATCH -J combine_{}_{}.job\n".format(sample,"FindSV"))
+        sbatch.write("#SBATCH -p core\n")
+        sbatch.write("#SBATCH -t 5:00:00\n")
+        sbatch.write("#SBATCH -n 1 \n")
+
+        sbatch.write("\n");
+        sbatch.write("\n");
+        sbatch.write("module load bioinfo-tools\n");
+        sbatch.write("module load vcftools\n");
+        vcfPath=[];
+        #sort each caller output file
+        for tool in combinedProcessFile:
+            files=combinedProcessFile[tool]["outputFile"]
+            files=files.strip().split(";")
+            
+            for vcf in files:
+                sortedvcf=os.path.join(combinedProcessFile[tools]["outpath"],tool,"sorted_"+vcf)
+                vcf=os.path.join(combinedProcessFile[tools]["outpath"],tool,vcf)
+                vcfPath.append(sortedvcf)
+                sbatch.write( "vcf-sort {} > {}\n".format(vcf,sortedvcf) ) 
+        #index and compress the files
+        fileString=''
+        for files in vcfPath:
+            #clear up from previous runs
+            sbatch.write( "rm {}.gz\n".format(files) )
+
+            sbatch.write( "{} bgzip {}\n".format(RTGpath,files) )
+            sbatch.write( "{} index -f vcf {}.gz\n".format(RTGpath,files) )
+            fileString += files+".gz "
+
+        #merge the files          
+        output=os.path.join(outpath,sample+".vcf")
+        sortedOutput=os.path.join(outpath,sample+".sorted.vcf")
+        sbatch.write("{} vcfmerge -o - -F {} > {}\n".format(RTGpath,fileString,output)) 
+        sbatch.write( "vcf-sort {} > {}\n".format(output,sortedOutput) )
+
+    pid = int(common.generateSlurmJob(sbatch_dir,sample))
+    add2Ongoing={sample:{"pid":pid,"outpath":combinedProcessFile[tools]["outpath"],"project":combinedProcessFile[tools]["project"],"outputFile":sample+".sorted.vcf"} };
+    
+    return (add2Ongoing);

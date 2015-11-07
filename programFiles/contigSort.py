dd1b1220c7fd542fef8dcbf4a65a66b5afb97a48
J35P312 <jesper.eisfeldt@scilifelab.se>
2015-11-08 00:02:08 +0100

    modified:   programFiles/contigSort.py

--- programFiles/contigSort.py
+++ programFiles/contigSort.py
@@ -5,22 +5,25 @@ def main(args,parser):
     header=subprocess.check_output("samtools view -H {}".format(args.bam), shell=True)
     header=header.split("\n")
     chromosome_order=[]
+    vcf_content={};
+    meta_data="";
     for line in header:
         content=line.split("\t")
         if(content[0] == "@SQ"):
             chromosome=content[1].strip().split(":")[1]
             chromosome_order.append(chromosome)
-    vcf_content={};
+            vcf_content[chromosome]=[];
     with open(args.vcf) as text_file:
         for line in text_file:
-            chromosome=line.split("\t")[0];
-            if chromosome in vcf_content:
-                vcf_content[chromosome].append(line);
+            if(line[0] == "#"):
+                meta_data+=line
             else:
-                vcf_content[chromosome]=[line]
+                chromosome=line.split("\t")[0];
+                vcf_content[chromosome].append(line);
+    print(meta_data.strip())
     for chromosome in chromosome_order:
         for line in vcf_content[chromosome]:
-            print(line)
+            print(line.strip())
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("""Uses samtools to read the header data of a bam file, accepts a vcf and sorts the contigs of the vcf according to the contig order, the output is printed to stdout""")

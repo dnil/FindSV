c50dbb3c66220ffb094a3f3d7223f7140cbc99b5
Jesper Eisfeldt <J35P312@users.noreply.github.com>
2016-02-20 19:16:34 +0100

    no restart
    FindSV will no longer restart as new samples are added to the database, the user will have to to so manually by using the restart module

--- modules/database.py
+++ modules/database.py
@@ -41,10 +41,6 @@ def buildDatabase(programDirectory,previousProcessFiles,processed,account):
             processFiles[tools]["ongoing"][sample]["pid"]=databaseOutput[0];
             processFiles[tools]["ongoing"][sample]["outputFile"]=databaseOutput[1];
             project=processFiles[tools]["ongoing"][sample]["project"]
-        if newsamples[tools]:
-            step={}
-            step["filter"]=True
-            process.restart(programDirectory,step,project,None)
 
     common.UpdateProcessFiles(processFiles,processed,"database")
 
